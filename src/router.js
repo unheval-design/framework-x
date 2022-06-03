e5b3a1a2cf5967cb6e2f30f9268fa21bce9a4195
omarbenjamin <benjaminomar@gmail.com>
2022-06-03 16:04:40 -0500

    Setup: basic structure and config router

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'welcome',
+            component: () => import('@/views/Welcome.vue')
+        }
+    ],
+})  
