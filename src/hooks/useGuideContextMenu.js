795120ab099682a2c0719c51aac580113726816a
omarbenjamin <benjaminomar@gmail.com>
2022-06-20 16:45:27 -0500

    Refactor: show guide custom context menu only when content is selected

--- src/hooks/useGuideContextMenu.js
+++ src/hooks/useGuideContextMenu.js
@@ -1,19 +1,25 @@
 import { ref, nextTick } from '@vue/runtime-core';
+import { useTextSelection } from '@vueuse/core';
+import { computed } from 'vue';
 
 const useGuideContextMenu = () => {
     const guideRef = ref();
+    const state = useTextSelection();
+    const selection = computed(() => state.text.value.trim());
+
     nextTick(() => {
         const contextMenu = document.getElementById('GuideContextMenu');
         guideRef.value.addEventListener('contextmenu', (event) => {
-            event.preventDefault();
+            if (selection.value) {
+                event.preventDefault();
+                const { clientX: mouseX, clientY: mouseY } = event;
 
-            const { clientX: mouseX, clientY: mouseY } = event;
+                contextMenu.style.top = `${mouseY}px`;
+                contextMenu.style.left = `${mouseX}px`;
 
-            contextMenu.style.top = `${mouseY}px`;
-            contextMenu.style.left = `${mouseX}px`;
-
-            contextMenu.classList.add('visible');
-            document.body.style.overflow = 'hidden';
+                contextMenu.classList.add('visible');
+                document.body.style.overflow = 'hidden';
+            }
         });
 
         document.body.addEventListener('click', (e) => {
