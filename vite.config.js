3f9f77b23939febd644bb364b80b5afecef67661
omarbenjamin <benjaminomar@gmail.com>
2022-07-11 14:32:10 -0500

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     },
-  },
-  plugins: [vue()]
-})
+    base: '/framework-x/',
+    plugins: [vue()]
+});
